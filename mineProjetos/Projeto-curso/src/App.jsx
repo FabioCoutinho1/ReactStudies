a7fab32f74096cf1f8ec407979840e2698886952
FabioCoutinho <fabiofarias549@gmail.com>
2025-04-12 18:11:42 -0300

    up

--- mineProjetos/Projeto-curso/src/App.jsx
+++ mineProjetos/Projeto-curso/src/App.jsx
@@ -1,11 +1,21 @@
-import './App.css'
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import Company from "./pages/Company";
+import NewProject from "./pages/NewProject";
 
 function App() {
   return (
-    <>
-     
-    </>
-  )
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Contact />} />
+        <Route path="/" element={<Company />} />
+        <Route path="/" element={<NewProject />} />
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
