753deb09f48c3fa84700a9ebcb6492c00b080c8d
FabioCoutinho <fabiofarias549@gmail.com>
2025-05-08 18:16:24 -0300

    Update Project.jsx

--- mineProjetos/Projeto-curso/src/components/pages/Project.jsx
+++ mineProjetos/Projeto-curso/src/components/pages/Project.jsx
@@ -1,9 +1,12 @@
 import { useParams } from "react-router-dom";
 import { useState, useEffect } from "react";
 
+import Load from "../layout/Load";
+
 const Project = () => {
   const { id } = useParams();
   const [project, setProject] = useState([]);
+  const [showForm, setShowForm] = useState(true);
 
   useEffect(() => {
     const getProject = async () => {
@@ -25,15 +28,47 @@ const Project = () => {
     getProject();
   }, [id]);
 
+  const toggleForm = () => {
+    setShowForm(!showForm);
+  };
   return (
     <>
-      <div className="flex">
-        <h1 className="text-4xl font-bold bg-black text-amber-300 p-4">
-          {project.name}
-        </h1>
+      {project.name ? (
+        <div className="border-b-2 border-b-gray-300">
+          <div className="flex justify-between mb-8">
+            <h1 className="text-4xl font-bold bg-black text-amber-300 p-4">
+              {project.name}
+            </h1>
+            <button
+              className="bg-gray-900 w-52 h-14 text-white transition duration-300 ease-in hover:text-amber-200 "
+              onClick={toggleForm}
+            >
+              {showForm ? "Editar Projeto" : "Fechar"}
+            </button>
+          </div>
 
-        <button>Editar Projeto</button>
-      </div>
+          {showForm ? (
+            <div className="mb-8">
+              <p>
+                <span className="font-bold">Categoria: </span>
+                {project.category.name}
+              </p>
+              <p>
+                <span className="font-bold">Tota de Orçamento: </span>R$
+                {project.buget}
+              </p>
+              <p>
+                <span className="font-bold">Total utilizaddo: </span>R$
+                {project.cost}
+              </p>
+            </div>
+          ) : (
+            "Form"
+          )}
+        </div>
+      ) : (
+        <Load />
+      )}
     </>
   );
 };
